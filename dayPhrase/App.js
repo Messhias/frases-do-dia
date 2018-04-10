ef92e0de88718ce88b0c81b4d7cb9d7d3514fa6f
Fabio William Conceição <fabiowilliamconceicao@MacBook-Air-de-Fabio.local>
2018-04-09 23:52:35 -0300

    88

--- dayPhrase/App.js
+++ dayPhrase/App.js
@@ -7,59 +7,34 @@
 import React, { Component } from 'react';
 import {
   Text,
-  View
+  View,
+  Button
 } from 'react-native';
 
 const styles = {
-    mainHeaderText: {
-        fontSize: 30,
-        backgroundColor: '#08509B',
-        color: 'white',
-        padding: 30,
-        fontWeight: '900',
-        shadowColor: '#000',
-        shadowOffset: {
-          width: 0,
-          height: 5
-        },
-        shadowOpacity: 0.75,
-        shadowRadius: 3
-    },
-    mainHeaderText2: {
-        fontSize: 30,
-        backgroundColor: '#2A48FA',
-        color: 'white',
-        padding: 30,
-        fontWeight: '900',
-        shadowColor: '#000',
-        shadowOffset: {
-          width: 0,
-          height: 5
-        },
-        shadowOpacity: 0.75,
-        shadowRadius: 3
-    },
-    estiloView: {
-        backgroundColor: 'skyblue',
-        height: 600,
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        flexDirection: 'column'
+    main: {
+        paddingTop: 40
     }
 };
 
+const botaoPressionado = () => {
+  alert('ok');
+}
+
 type Props = {};
 export default class App extends Component<Props> {
+
+
   render() {
-    const { mainHeaderText, mainHeaderText2, estiloView } = styles;
+    const { main } = styles;
     return (
-      <View style={estiloView}>
-        <Text style={mainHeaderText}>
-          Frases do dia
-        </Text>
-        <Text style={mainHeaderText2}>
-          Frases do dia2
-        </Text>
+      <View style={main}>
+          <Button
+            onPress={botaoPressionado}
+            color="#841584"
+            accessibilityLabel="Clica ai"
+            title="Clica ai"
+          />
       </View>
     );
   }
