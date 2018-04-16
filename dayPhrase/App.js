7d3a1073d267b6b65d4638337c747707dcb6e5c6
Fabio William Conceição <fabiowilliamconceicao@MacBook-Air-de-Fabio.local>
2018-04-16 00:22:23 -0300

    application complete

--- dayPhrase/App.js
+++ dayPhrase/App.js
@@ -6,35 +6,70 @@
 
 import React, { Component } from 'react';
 import {
-  Text,
   View,
-  Button
+  Image,
+  TouchableOpacity,
+  Text,
+  Alert
 } from 'react-native';
 
 const styles = {
     main: {
-        paddingTop: 40
+      justifyContent: 'center',
+      alignItems: 'center',
+      flex: 1
+    },
+    botao: {
+      backgroundColor: '#538530',
+      paddingVertical: 10,
+      paddingHorizontal: 40,
+      marginTop: 20
+    },
+    textoBotao: {
+      color: 'white',
+      fontSize: 16,
+      fontWeight: 'bold'
     }
 };
 
-const botaoPressionado = () => {
-  alert('ok');
-}
+const createPhrase = () => {
+  let randomNumber = Math.random();
+  // phrases
+  const phrases = [
+    'A partida só acaba quando terminar.',
+    'Vencedor é aquele que ganha',
+    'Ganhar é o oposto de perder',
+    'Perder é o oposto de ganhar'
+  ];
+  randomNumber = Math.floor(randomNumber * 5);
+
+
+  const phraseChosen = phrases[randomNumber];
+
+  Alert.alert(phraseChosen);
+};
 
 type Props = {};
 export default class App extends Component<Props> {
 
 
   render() {
-    const { main } = styles;
+    const {
+      main,
+      botao,
+      textoBotao
+    } = styles;
     return (
       <View style={main}>
-          <Button
-            onPress={botaoPressionado}
-            color="#841584"
-            accessibilityLabel="Clica ai"
-            title="Clica ai"
-          />
+        <Image source={require('./imagem/logo.png')} />
+        <TouchableOpacity
+          onPess={createPhrase}
+          style={botao}>
+          <Text style={textoBotao}>
+            Nova Frase
+          </Text>
+        </TouchableOpacity>
+
       </View>
     );
   }
